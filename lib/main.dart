3d2db08b835e2d9d26a9255c2b22fd4aec518a6d
Carl Einarsson <carl@flowbic.se>
2021-03-13 14:33:25 +0100

    inital, just some test code

---
+++ lib/main.dart
@@ -0,0 +1,269 @@
+import 'package:device_frame/device_frame.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _page = 0;
+
+  PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: _page);
+    super.initState();
+  }
+
+  switchScreen(newPage) => setState(() => this._page = newPage);
+
+  @override
+  Widget build(BuildContext context) {
+    /* Bottom Navigation */
+    renderBottomNavigationItems() => <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.wifi_tethering),
+            label: 'Sida 1',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'Sida två',
+          ),
+        ];
+
+    renderBottomNavigation() => BottomNavigationBar(
+          onTap: (index) => this._pageController.jumpToPage(index),
+          type: BottomNavigationBarType.fixed,
+          items: renderBottomNavigationItems(),
+        );
+
+    Widget deviceFrame = DeviceFrame(
+      device: Devices.ios.iPhone11,
+      isFrameVisible: true,
+      orientation: Orientation.portrait,
+      screen: Scaffold(
+        appBar: AppBar(
+          title: Text('Frobics'),
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: (newPage) => switchScreen(newPage),
+          children: [
+            ScreenOne(),
+            ScreenTwo(),
+          ],
+        ),
+        bottomNavigationBar: renderBottomNavigation(),
+      ),
+    );
+
+    Widget leftSideWelcomeText = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Flowbic',
+          style: TextStyle(fontSize: 40),
+        ),
+        Text(
+          'Vi super bra, du supernöjd',
+          style: TextStyle(fontSize: 30),
+        ),
+      ],
+    );
+
+    renderHeroSection() => Container(
+          color: Color(0xffa000),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth < 1000) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    leftSideWelcomeText,
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: deviceFrame,
+                    ),
+                  ],
+                );
+              }
+
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  leftSideWelcomeText,
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.75,
+                    child: deviceFrame,
+                  ),
+                ],
+              );
+            },
+          ),
+        );
+
+    renderAboutTeamSection() => Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth < 1000) {
+                return Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.25,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.red,
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            'THE TEAM',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          const SizedBox(height: 10),
+                          Text(
+                            'Tillit, lyhördhet och ett starkt gemensamt intresse är det som definierar Flowbic. Det har gett oss kunskapen om varandras styrkor och svagheter. Vi vet vem som kan mest om problemet vi har framför oss. Stark gruppdynamik och nära kommunikation gör att vi kan växa tillsammans för varje individuell prestation.',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 15),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.25,
+                      width: MediaQuery.of(context).size.width,
+                      child: Image.asset(
+                        'lib/assets/images/team.jpeg',
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  ],
+                );
+              }
+
+              return Row(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'THE TEAM',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 30),
+                        ),
+                        Text(
+                          'Tillit, lyhördhet och ett starkt gemensamt intresse är det som definierar Flowbic. Det har gett oss kunskapen om varandras styrkor och svagheter. Vi vet vem som kan mest om problemet vi har framför oss. Stark gruppdynamik och nära kommunikation gör att vi kan växa tillsammans för varje individuell prestation.',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    padding: EdgeInsets.all(50),
+                    child: Image.asset('lib/assets/images/team.jpeg'),
+                  )
+                ],
+              );
+            },
+          ),
+        );
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            renderHeroSection(),
+            renderAboutTeamSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ScreenTwo extends StatelessWidget {
+  const ScreenTwo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: ListView(
+      padding: EdgeInsets.all(50),
+      children: [
+        Image.asset(
+          'lib/assets/images/edaierlogo.png',
+          height: 200,
+        ),
+        const SizedBox(height: 40),
+        Image.asset(
+          'lib/assets/images/OhCleo_logo.png',
+          height: 200,
+        ),
+        const SizedBox(height: 40),
+        Image.asset('lib/assets/images/giddslogo.png', height: 100),
+      ],
+    ));
+  }
+}
+
+class ScreenOne extends StatelessWidget {
+  const ScreenOne({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset('lib/assets/images/flowbiclogo.png'),
+          const SizedBox(height: 20),
+          Text(
+            'Webb & Mobilutvecklare :P',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          )
+        ],
+      ),
+    );
+  }
+}
